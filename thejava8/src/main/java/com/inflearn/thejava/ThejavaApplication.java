97376351f0249d282e767ad419a7f1f9ceb4e680
Han <quaint3@gmail.com>
2020-08-20 21:00:59 +0900

    feat : the java 8 project init
    - 인프런 강좌 the java 프로젝트 생성

---
+++ thejava8/src/main/java/com/inflearn/thejava/ThejavaApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.thejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThejavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThejavaApplication.class, args);
+  }
+
+}
---
+++ thejava8/src/test/java/com/inflearn/thejava/ThejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.thejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThejavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
