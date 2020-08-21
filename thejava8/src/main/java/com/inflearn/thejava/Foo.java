1115bd2b0d72c9f493f4943ecea3eac3b2d9303a
Han <quaint3@gmail.com>
2020-08-21 15:02:03 +0900

    feat : 1-3
    - 람다 표현식

--- thejava8/src/main/java/com/inflearn/thejava/Foo.java
+++ thejava8/src/main/java/com/inflearn/thejava/Foo.java
@@ -1,12 +1,36 @@
 package com.inflearn.thejava;
 
-import java.util.function.Predicate;
+import java.util.function.IntConsumer;
 
 public class Foo {
 
   public static void main(String[] args) {
-    Predicate<String> start = (s) -> s.startsWith("han");
-    Predicate<Integer> isOdd = (i) -> i % 2 == 1;
+    Foo foo = new Foo();
+    foo.run();
+  }
+
+  private void run() {
+    final int baseNumber = 10;
+
+    //로컬 클래스
+    class LocalClass{
+      void printBaseNumber() {
+        int baseNumber = 11;
+        System.out.println(baseNumber); // 11이 찍힘. 즉 로컬 클래스내 baseNumber가 로컬 변수인 baseNumber을 가리고 있다.
+      }
+    }
+
+    IntConsumer intConsumer = new IntConsumer() {
+      @Override
+      public void accept(int baseNumber) { //매개변수로 들어간 baseNumber가 로컬 변수를 가린다.
+        System.out.println(baseNumber); //익명 클래스에서 로컬 변수를 참조하고 있음.
+      }
+    };
+
+    IntConsumer printInt = (i) -> {
+      System.out.println(i); //람다에서 로컬 변수를 참조하고 있음.
+    };
+    printInt.accept(10);
   }
 
 }
