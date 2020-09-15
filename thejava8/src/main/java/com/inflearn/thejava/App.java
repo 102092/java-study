5ad190cd8805e41aa633b5589796e67ff9413f40
102092 <quaint3@gmail.com>
2020-09-16 00:07:16 +0900

    update project
    modify build.gradle of java targetSource 11 to 8
    delete some class file

--- thejava8/src/main/java/com/inflearn/thejava/App.java
+++ thejava8/src/main/java/com/inflearn/thejava/App.java
@@ -1,29 +1,17 @@
 package com.inflearn.thejava;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.Date;
 
 public class App {
 
-  public static void main(String[] args) {
-    List<OnlineClass> springClasses = new ArrayList<>();
-    springClasses.add(new OnlineClass(1, "spring boot", true));
-    springClasses.add(new OnlineClass(2, "spring data jpa", true));
-    springClasses.add(new OnlineClass(3, "spring mvc", false));
-    springClasses.add(new OnlineClass(4, "spring core", false));
-    springClasses.add(new OnlineClass(5, "rest api development", false));
+  public static void main(String[] args) throws InterruptedException {
+      Date date = new Date();
+      long time = date.getTime();
 
-    Optional<OnlineClass> spring = springClasses.stream()
-        .filter(oc -> oc.getTitle().startsWith("spring")).findFirst();
-
-    Optional<Progress> progress = spring.flatMap(OnlineClass::getProgress);
-    System.out.println(progress.isEmpty());
-
-  }
-
-  private static OnlineClass createNewClass() {
-    System.out.println("creating new class...");
-    return new OnlineClass(10, "New Class", false);
+      Thread.sleep(1000 * 3);
+      Date after3Seconds = new Date();
+      System.out.println(after3Seconds);
+      after3Seconds.setTime(time);
+      System.out.println(after3Seconds);
   }
 }
