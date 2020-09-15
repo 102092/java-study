723cd641b7875c82668d69e664e9713889fc93cd
102092 <quaint3@gmail.com>
2020-09-16 00:40:42 +0900

    thisisjava8 5-1 date/time API
    summary java8 date/time API

--- thejava8/src/main/java/com/inflearn/thejava/App.java
+++ thejava8/src/main/java/com/inflearn/thejava/App.java
@@ -1,17 +1,31 @@
 package com.inflearn.thejava;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class App {
 
-  public static void main(String[] args) throws InterruptedException {
-      Date date = new Date();
-      long time = date.getTime();
+  public static void main(String[] args) {
+      Date date = new Date(); //레거시 API
+      Instant instant = date.toInstant(); //레거시 API to java8 time API
+      Date newDate = Date.from(instant); //java8 Time API to 레거시 API
 
-      Thread.sleep(1000 * 3);
-      Date after3Seconds = new Date();
-      System.out.println(after3Seconds);
-      after3Seconds.setTime(time);
-      System.out.println(after3Seconds);
+      GregorianCalendar gregorianCalendar = new GregorianCalendar(); // 이전 그레고리 캘린더 인스턴스를
+      LocalDateTime dateTime1 = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(); // LocalDateTime으로 바꿀 수 있음.
+      ZonedDateTime dateTime2 = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault());
+      GregorianCalendar from = GregorianCalendar.from(dateTime2);
+
+      ZoneId zoneId = TimeZone.getTimeZone("PST").toZoneId(); //최근
+      TimeZone timeZone  = TimeZone.getTimeZone(zoneId); //레거시
+
+      LocalDateTime now = LocalDateTime.now();
+      LocalDateTime plus = now.plus(10, ChronoUnit.MINUTES);
+      System.out.println(plus); //현재 시간에서 10분이 추가되었음.
   }
 }
