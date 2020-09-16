6a4b8e55e53d66baef0f8216e08b1779bf808ff7
Han <quaint3@gmail.com>
2020-09-16 21:36:29 +0900

    thisisjava8 6-1 Thread intro

--- thejava8/src/main/java/com/inflearn/thejava/App.java
+++ thejava8/src/main/java/com/inflearn/thejava/App.java
@@ -1,31 +1,19 @@
 package com.inflearn.thejava;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-
 public class App {
 
-  public static void main(String[] args) {
-      Date date = new Date(); //레거시 API
-      Instant instant = date.toInstant(); //레거시 API to java8 time API
-      Date newDate = Date.from(instant); //java8 Time API to 레거시 API
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(3000L); //1초간 Thread를 잠재운다.
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
 
-      GregorianCalendar gregorianCalendar = new GregorianCalendar(); // 이전 그레고리 캘린더 인스턴스를
-      LocalDateTime dateTime1 = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(); // LocalDateTime으로 바꿀 수 있음.
-      ZonedDateTime dateTime2 = gregorianCalendar.toInstant().atZone(ZoneId.systemDefault());
-      GregorianCalendar from = GregorianCalendar.from(dateTime2);
-
-      ZoneId zoneId = TimeZone.getTimeZone("PST").toZoneId(); //최근
-      TimeZone timeZone  = TimeZone.getTimeZone(zoneId); //레거시
-
-      LocalDateTime now = LocalDateTime.now();
-      LocalDateTime plus = now.plus(10, ChronoUnit.MINUTES);
-      System.out.println(plus); //현재 시간에서 10분이 추가되었음.
-  }
+        });
+        thread.start();
+        System.out.println("Hello: " + Thread.currentThread().getName());
+        thread.join(); //main thread가 thread-0이 3초후 끝날 때 까지 기다렸다가, 진행된다
+        System.out.println(thread + "is finished");
+    }
 }
