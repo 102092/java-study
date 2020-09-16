d2fa8ada871909ec4e583b3143b9896eecaabb1b
Han <quaint3@gmail.com>
2020-09-16 21:53:47 +0900

    thisisjava8 6-2 Executors intro
    how to use ExecutorService

--- thejava8/src/main/java/com/inflearn/thejava/App.java
+++ thejava8/src/main/java/com/inflearn/thejava/App.java
@@ -1,19 +1,18 @@
 package com.inflearn.thejava;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(() -> {
-            try {
-                Thread.sleep(3000L); //1초간 Thread를 잠재운다.
-            } catch (InterruptedException e) {
-                throw new IllegalStateException(e);
-            }
+    public static void main(String[] args) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(getRunner("apple"), 1,2, TimeUnit.SECONDS);
+//        executorService.shutdown();
+    }
 
-        });
-        thread.start();
-        System.out.println("Hello: " + Thread.currentThread().getName());
-        thread.join(); //main thread가 thread-0이 3초후 끝날 때 까지 기다렸다가, 진행된다
-        System.out.println(thread + "is finished");
+    private static Runnable getRunner(String apple) {
+        return () -> System.out.println(apple + ": " + Thread.currentThread().getName());
     }
 }
