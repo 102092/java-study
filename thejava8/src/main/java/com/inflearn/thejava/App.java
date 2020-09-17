43bc4b62094fe23c59144d8f61decc353ff8eb38
Han <quaint3@gmail.com>
2020-09-17 22:17:37 +0900

    thisisjava8 6-4 CompletableFuture
    outside can send signal of stop to Future

--- thejava8/src/main/java/com/inflearn/thejava/App.java
+++ thejava8/src/main/java/com/inflearn/thejava/App.java
@@ -1,31 +1,17 @@
 package com.inflearn.thejava;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class App {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-
-        Callable<String> hello = () -> {
-            Thread.sleep(2000L);
-            return "hello";
-        };
-
-        Callable<String> java = () -> {
-            Thread.sleep(3000L);
-            return "java";
-        };
-
-        Callable<String> keesun = () -> {
-            Thread.sleep(1000L);
-            return "keesun";
-        };
-
-        String s = executorService.invokeAny(Arrays.asList(hello, java, keesun)); //blocking call, 바로 결과가 나옴.
-        System.out.println(s);
-        executorService.shutdown();
+        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello " + Thread.currentThread().getName());
+            return "Hello";
+        }).thenRun(() -> {
+            System.out.println(Thread.currentThread().getName()); //Runnable
+        });
+        future.get();
     }
 }
